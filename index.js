7beaee5742e30dabb1b1bc4ad84afbf8a03aee92
Rakshith658 <rakshithkumars7777@gmail.com>
2021-06-21 23:07:00 +0530

    addeing socketio is remaining

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const multer = require("multer")
+const path = require("path")
+
 const userRoute = require('./Routes/users')
 const authRoute = require('./Routes/auth');
 const postRoute = require('./Routes/posts');
+const convarsationRoute = require('./Routes/convarsation');
+const messagesRoute = require('./Routes/messages');
 
 
 dotenv.config();
@@ -24,8 +29,31 @@ app.use(express.json())
 app.use(helmet())
 app.use(morgan('common'))
 
+// app.use("/images",express.static(path.join(__dirname,"public/image")))
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
+const storage=multer.diskStorage({
+  destination:(req,file,cb)=>{
+    cb(null,"public/images")
+  },
+  filename:(req,file,cb)=>{
+    cb(null,file.originalname)
+  }
+})
+
+const upload = multer({storage})
+app.post('/api/upload',upload.single("file"),(req,res)=>{
+  try {
+    return res.status(200).json("File uploaded successfully")
+  } catch (error) {
+    console.log(error);
+  }
+})
+
 app.use('/api/users',userRoute)
 app.use('/api/auth',authRoute)
 app.use('/api/posts',postRoute)
+app.use('/api/convarsation',convarsationRoute)
+app.use('/api/messages',messagesRoute)
 
 app.listen(5050,()=>{console.log('Server is lestening to port number 5050')})
